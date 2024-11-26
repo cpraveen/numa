f623bf9adf7b50fda2b1bb61ee1bd3bf9cac9c21
Praveen C <cpraveen@gmail.com>
2024-11-26 11:21:30 +0530

    Add algos

---
+++ latex/run.sh
@@ -0,0 +1,12 @@
+rm -f *.pdf
+pdflatex main.tex
+
+npages=3
+
+for ((i = 1 ; i <= npages ; i++ ))
+do 
+   echo "------------------------$i------------------------"
+   pdfjam main.pdf $i -o p$i.pdf
+   pdfbb p$i.pdf
+   pdftocairo -svg p$i.pdf p$i.svg
+done
